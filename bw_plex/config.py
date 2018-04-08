bc785f259f2b88f1f585c9245cd99ac001175da9
Hellowlol <hellowlol1@gmail.com>
2018-04-08 02:06:29 +0200

    add comment with the diff options.

--- bw_plex/config.py
+++ bw_plex/config.py
@@ -27,6 +27,7 @@ users = list(default=list())
 # List of client names, empty list allows all
 clients = list(default=list())
 mode = string(default='skip_only_theme')
+# possible values are youtube, plex, tvtunes or all
 theme_source = string(default='youtube')
 """.splitlines()
 
