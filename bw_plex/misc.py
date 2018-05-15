a895fa21bf65d8effa2f589b726c3a55ca7b7e4e
Hellowlol <hellowlol1@gmail.com>
2018-05-15 19:23:25 +0200

    remove duplicate match windows.

--- bw_plex/misc.py
+++ bw_plex/misc.py
@@ -175,12 +175,15 @@ def calc_offset(final_video, final_audio, dev=7, cutoff=15):
             return -1
 
     if match_window:
+         # remove dupes
+        match_window = set(tuple(i) for i in match_window)
+
         try:
-            # Sort on end time and duration.
-            # TODO remove close matches and attempt to get the second best?
+           # Sort on end time and duration.
             m = list(sorted(match_window, key=lambda k: (k[1], k[2])))
             LOG.debug('Matching windows are %s', to_time_range(m))
-            LOG.debug('RAW %r', match_window)
+            # So this might be wrong on some shows as as they fade to black and have audio silence
+            # before the theme song.
             return m[0][0]
         except IndexError:
             return -1
