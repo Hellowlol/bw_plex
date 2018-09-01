790218026c6953a97eb2c5b2cc504613cb733699
Hellowlol <hellowlol1@gmail.com>
2018-09-02 00:31:36 +0200

    add method to convert edl to metadata file.

--- bw_plex/edl.py
+++ bw_plex/edl.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+import time
+
 
 TYPES = {'cut': '0',
          'mute': '1',
@@ -107,3 +110,67 @@ def write_edl(path, lines):
 
     return path
 
+
+def write_chapters_to_file(path, cleanup=False):
+
+    edl = has_edl(path)
+    if edl:
+        mf_file = edl_to_metadata_file(path)
+
+    outfile, ext = os.path.splitex(path)
+    outfile = outfile + '__bw_plex_meta' + ext
+
+    cmd = ['ffmpeg', '-i', path, '-i', mf_file, '-map_metadata', '1', '-copy', 'copy', outfile]
+
+    proc = subprocess.Popen(cmd)
+    code = proc.wait()
+    if code != 0:
+        print(code)
+
+    for i in range(3):
+        try:
+            os.rename(path, path.replace('__bw_plex_meta', ''))
+            break
+        except OSError:
+            time.sleep(1)
+
+    if cleanup:
+        os.remove(mf_file)
+
+    return path
+
+
+def edl_to_metadata_file(path):
+    # Should we check if this file has metadata/chapters so we dont overwrite it
+    # Lets come back to this later.
+    if not os.path.isfile(path) and path.endswith('.edl'):
+        return
+
+    header = ';FFMETADATA1\ntitle=%s\nartist=Made by bw_plex\n\n' % os.path.splitext(os.path.basename(path))[0]
+
+    chapter_template = """[CHAPTER]\nTIMEBASE=1/1000\nSTART=%s\nEND=%s\ntitle=%s\n\n"""
+
+    meta_name = os.path.splitext(path)[0] + '.metadata'
+
+    with open(path) as e:
+        lines = e.readlines()
+        lines = [i.split() for i in lines if i]
+
+    with open(meta_name, 'w') as mf:
+        last_en = len(lines) - 1
+        mf.write(header)
+        for en, l in enumerate(lines):
+            if en == 0:
+                title = 'first'
+            elif en == last_en:
+                title = 'last'
+            else:
+                title = ''
+
+            mf.write(chapter_template % (int(l[0]) * 1000, int(l[1]) * 1000, title))
+
+    return meta_name
+
+
+if __name__ == '__main__':
+    edl_to_metadata_file(r'C:\Users\alexa\.config\bw_plex\test.edl')
