f7f7eec63cbb31ef3b86adebbe8f257aad7c3d22
Hellowlol <Hellowlol@users.noreply.github.com>
2019-07-06 17:28:42 +0200

    fix test for windows.

--- tests/test_edl.py
+++ tests/test_edl.py
@@ -55,7 +55,13 @@ def test_write_chapters_to_file(intro_file, tmpdir, monkeypatch, mock):
     else:
         fn = str(intro_file)
     # Confirm that no chapters exists in that file.
-    cmd = ['ffprobe', '-i', fn, '-print_format', 'json', '-show_chapters', '-loglevel', 'error']
+    base_cmd = ['ffprobe', '-i', fn, '-print_format', 'json', '-show_chapters', '-loglevel', 'error']
+
+    if os.name == 'nt':
+        cmd = '%s' % ' '.join(base_cmd)
+    else:
+        cmd = base_cmd
+
     assert check_chapter(cmd) is False
 
     f = tmpdir.join('hello.mkv')
@@ -67,7 +73,7 @@ def test_write_chapters_to_file(intro_file, tmpdir, monkeypatch, mock):
     edl_file = edl.write_edl('hello.mkv', [[1, 2, 3]])
     modified_file = edl.write_chapters_to_file(str(f), input_edl=edl_file)
 
-    new_cmd = list(cmd)
+    new_cmd = list(base_cmd)
 
     if os.name == 'nt':
         fn_mod = '"%s"' % str(modified_file)
@@ -75,5 +81,8 @@ def test_write_chapters_to_file(intro_file, tmpdir, monkeypatch, mock):
         fn_mod = str(modified_file)
 
     new_cmd[2] = fn_mod
+
+    if os.name == 'nt':
+        new_cmd = '%s' % ' '.join(new_cmd)
 
     assert check_chapter(new_cmd)
